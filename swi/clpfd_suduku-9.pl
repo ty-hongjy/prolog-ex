1b252d9eaec90c5f83403bdbc692a411ad875f01
ty-hongjy <hongjy1@163.com>
2021-05-25 17:06:58 +0800

    2021-05-25

--- swi/clpfd_suduku-9.pl
+++ swi/clpfd_suduku-9.pl
@@ -11,8 +11,8 @@ sudoku(Rows) :-
 	Rows = [A,B,C,D,E,F,G,H,I],
 	blocks(A, B, C), blocks(D, E, F), blocks(G, H, I).
 
-	blocks([], [], []).
-	blocks([A,B,C|Bs1], [D,E,F|Bs2], [G,H,I|Bs3]) :-
+blocks([], [], []).
+blocks([A,B,C|Bs1], [D,E,F|Bs2], [G,H,I|Bs3]) :-
 	all_distinct([A,B,C,D,E,F,G,H,I]),
 	blocks(Bs1, Bs2, Bs3).
 
