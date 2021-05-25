c270e7971bec1eddfaa2ea70685bba91582f47a9
ty-hongjy <hongjy1@163.com>
2021-05-26 06:44:16 +0800

    2021-06-14

--- hanoi.pl
+++ hanoi.pl
@@ -2,9 +2,15 @@ hanoi(N) :- move(N,left,middle,right).
 
 move(1,A,_,C) :- inform(A,C), !.
 move(N,A,B,C) :- N1 is N - 1,
+	write(N),
 	move(N1,A,C,B),
-	inform(A,C),
+	write(N1),
+	move(1,A,_,C),
+	%inform(A,C),
+	%inform1(N1),
 	move(N1,B,A,C).
 
 inform(Loc1,Loc2) :- nl,write('Move a disk from '-Loc1-' to '-Loc2).
+%inform1(disk) :- nl,write('disk: '-disk-).
+inform1(disk) :- write(disk).
 
