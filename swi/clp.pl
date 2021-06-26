d8cf5fab565ea406e0ae3e1c2e49f11292333a24
ty-hongjy <hongjy1@163.com>
2021-06-26 11:34:06 +0800

    2021-06-26

---
+++ swi/clp.pl
@@ -0,0 +1,18 @@
+% Constraint Logic Programming
+:- use_module(library(dif)).	% Sound inequality
+:- use_module(library(clpfd)).	% Finite domain constraints
+:- use_module(library(clpb)).	% Boolean constraints
+:- use_module(library(chr)).	% Constraint Handling Rules
+:- use_module(library(when)).	% Coroutining
+:- use_module(library(clpq)).  % Constraints over rational numbers
+:- use_module(library(assoc)).  % Constraints over rational numbers
+:- use_module(library(record)).  % Constraints over rational numbers
+%:- use_module(library(clph)).  % Constraints over rational numbers
+
+% Your program goes here
+
+%:- X #= 1+2.
+/** <examples> Your example queries go here, e.g.
+?- X #> 1.
+*/
+
